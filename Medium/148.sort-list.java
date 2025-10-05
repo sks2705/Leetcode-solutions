6cd237253a7e839c2dddc36c085f7616b7f2e11f
sks2705 <shivamkumarsingh2704@gmail.com>
2025-10-05 21:46:26 +0530

    sorting

---
+++ Medium/148.sort-list.java
@@ -0,0 +1,52 @@
+//Given the head of a linked list, return the list after sorting it in ascending order.
+
+//https://leetcode.com/problems/sort-list
+
+class Solution {
+    public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode mid = getMid(head);
+        ListNode left = sortList(head);
+        ListNode right = sortList(mid);
+
+        return merge(left, right);
+    }
+
+    ListNode getMid(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        ListNode prev = null;
+
+        while (fast != null && fast.next != null) {
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        if (prev != null) {
+            prev.next = null; // split the list into two
+        }
+        return slow; // mid node
+    }
+
+    ListNode merge(ListNode list1, ListNode list2) {
+        ListNode dummyHead = new ListNode();
+        ListNode tail = dummyHead;
+
+        while (list1 != null && list2 != null) {
+            if (list1.val < list2.val) {
+                tail.next = list1;
+                list1 = list1.next;
+            } else {
+                tail.next = list2;
+                list2 = list2.next;
+            }
+            tail = tail.next;
+        }
+
+        tail.next = (list1 != null) ? list1 : list2;
+        return dummyHead.next;
+    }
+}
