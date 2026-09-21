9244724fa4442245a100cec705416529c201249f
ShivamKSingh <167811836+sks2705@users.noreply.github.com>
2026-09-21 22:38:04 +0530

    Time: 8 ms (24.79%), Space: 47.4 MB (21.08%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,27 +1,29 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-        for(int i =0;i<numCourses;i++){
+        List<ArrayList<Integer>> adj = new ArrayList<>();
+        for(int i=0;i<numCourses;i++){
             adj.add(new ArrayList<>());
         }
-        for(int i =0;i<prerequisites.length;i++){
+        for(int i=0;i<prerequisites.length;i++){
             adj.get(prerequisites[i][1]).add(prerequisites[i][0]);
         }
+
         int [] indegree=new int[numCourses];
-        for(int i =0;i<numCourses;i++){
+        for(int i=0;i<numCourses;i++){
             for(int it:adj.get(i)){
                 indegree[it]++;
             }
         }
-        Queue<Integer> q = new LinkedList<>();
+
+        Queue<Integer>q=new LinkedList<>();
         for(int i =0;i<numCourses;i++){
             if(indegree[i]==0){
                 q.offer(i);
             }
         }
-        List<Integer>topo= new ArrayList<>();
+        List<Integer> topo = new ArrayList<>();
         while(!q.isEmpty()){
-            int node = q.peek();
+            int node=q.peek();
             q.remove();
             topo.add(node);
             for(int it:adj.get(node)){
@@ -30,10 +32,11 @@ class Solution {
                     q.add(it);
                 }
             }
-        }   
-        if(topo.size()==numCourses) {
+
+        }
+        if(topo.size()==numCourses){
             return true;
-            }
+        }
         return false;
     }
 }
